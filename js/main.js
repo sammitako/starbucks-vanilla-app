214f0a4469925bea1cdb9637dfbc25133e33d7d7
sammitako <p.hansaem@hotmail.com>
2021-12-21 18:35:21 +0900

    badge

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+// 스크롤이 어떤 값 이상으로 커지면 배지를 사라지게 한다.
+const badgeEl = document.querySelector('header .badges');
+
+// 화면자체
+window.addEventListener('scroll', _.throttle(function() { // 일정 시간 마다 함수가 실행되게 제한을 두는 js 애니메이션 라이브러리
+  if(window.scrollY > 500) {
+    // 배지 숨기기
+    // badgeEl.style.display = 'none';
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    }); // 요소, 지속시간, {옵션}
+  } else {
+    // 배지 보이기
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
