5c0e3e87586ddf2e05a967a7b3fada3ad4c0d5da
sammitako <p.hansaem@hotmail.com>
2021-12-27 08:43:38 +0900

    Youtube

--- js/main.js
+++ js/main.js
@@ -50,3 +50,76 @@ fadeEls.forEach(function(fadeEl, index) {
     opacity: 1
   });
 });
+
+
+// 공지사항 스와이퍼
+// (선택자, {옵션})
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+}); 
+
+// 프로모션 스와이퍼
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백 픽셀
+  centeredSlides: true, // 첫번째 슬라이드가 가운데 위치
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+// Promotion Toggle
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
+
+// 램덤한 숫자를 생성하는 함수
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+// 둥둥 떠다니는 스티커
+function floatingObject(selector, delay, size) {
+  // 애니메이션 삽입
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+      y: size,
+      repeat: -1, // 무한반복
+      yoyo: true, // 한번 재생하고 다시 뒤로 재생해서 돌아오도록
+      ease: Power1.easeInOut, // Easing 함수
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
